5ed5e55d2495b33cc77f98fac672dc3cc8887811
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-06-12 18:58:10 -0400

    started implementing heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,36 @@
+import math
 class Heap:
   def __init__(self):
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
 
+    for i in range(1,len(self.storage),-1):
+      print(i)
+      
+    
   def delete(self):
     pass
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    if self.storage[index] > self.storage[math.floor((index-1)/2)]:
+      temp = self.storage[index]
+      self.storage[index] = self.storage[math.floor((index-1)/2)]
+      self.storage[math.floor((index-1)/2)] = temp
 
   def _sift_down(self, index):
-    pass
+    l = 2*index + 1
+    r = 2*index + 2
+    greater_index = l if self.storage[l] > self.storage[r] else r
+    if self.storage[index] < self.storage[greater_index]:
+      temp = self.storage[index]
+      self.storage[index] = self.storage[greater_index]
+      self.storage[greater_index] = temp
+
