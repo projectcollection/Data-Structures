466a3b9bf98e2b9b860dacdb538bcfcead6d8966
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-06-13 03:04:12 -0400

    max_heap done

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -5,32 +5,47 @@ class Heap:
 
   def insert(self, value):
     self.storage.append(value)
-
-    for i in range(1,len(self.storage),-1):
-      print(i)
-      
+    self._sift_down(0)
+    self._bubble_up(len(self.storage) - 1)
+    self._bubble_up(len(self.storage) - 2)
     
   def delete(self):
-    pass
+    deleted = self.storage.pop(0)    
+    if len(self.storage) > 0:
+      self.insert(self.storage.pop())
+    return deleted
 
   def get_max(self):
-    return self.storage[0]
+    return self.storage[0] if len(self.storage) > 0 else None
 
   def get_size(self):
     return len(self.storage)
 
   def _bubble_up(self, index):
-    if self.storage[index] > self.storage[math.floor((index-1)/2)]:
-      temp = self.storage[index]
-      self.storage[index] = self.storage[math.floor((index-1)/2)]
-      self.storage[math.floor((index-1)/2)] = temp
+    if(index < 1):
+      return
+    if math.floor((index-1)/2) >= 0:
+      if self.storage[index] > self.storage[math.floor((index-1)/2)]:
+        temp = self.storage[index]
+        self.storage[index] = self.storage[math.floor((index-1)/2)]
+        self.storage[math.floor((index-1)/2)] = temp
+        self._bubble_up(math.floor((index-1)/2))
+      else:
+        return
+    else:
+      return
 
   def _sift_down(self, index):
     l = 2*index + 1
     r = 2*index + 2
-    greater_index = l if self.storage[l] > self.storage[r] else r
-    if self.storage[index] < self.storage[greater_index]:
-      temp = self.storage[index]
-      self.storage[index] = self.storage[greater_index]
-      self.storage[greater_index] = temp
-
+    if l < len(self.storage) and r < len(self.storage):
+      greater_index = l if self.storage[l] > self.storage[r] else r
+      if self.storage[index] < self.storage[greater_index]:
+        temp = self.storage[index]
+        self.storage[index] = self.storage[greater_index]
+        self.storage[greater_index] = temp
+        self._sift_down(l if greater_index == l else r)
+      else:
+        return
+    else:
+      return
